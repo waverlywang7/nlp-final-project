f4d7de587a50cf00b0c320295f87c9f36adac96d
Adam Cohan <adam141d@gmail.com>
2023-04-21 14:46:17 -0700

    made the ngram class to use for the type for the key for hashmaps

---
+++ code/nlp/lm/NGram.java
@@ -0,0 +1,25 @@
+package code.nlp.lm;
+
+import java.util.ArrayList;
+
+public class NGram {
+    
+    private int n;
+    private ArrayList<String> words;
+
+
+    public NGram(ArrayList<String> ngram) {
+        n = ngram.size();
+        for(String word : ngram) {
+            words.add(word);
+        }
+    }
+
+    public ArrayList<String> getN_1Gram() {
+        return (ArrayList) words.subList(0, n-1);
+    }
+
+    public ArrayList<String> getNGram() {
+        return this.words;
+    }
+}
