d57ec7ce54173111d8fc455829c568b41f37bd91
Adam Cohan <adam141d@gmail.com>
2023-04-25 15:13:38 -0700

    added NGramCount and N_1GramCounts

--- code/nlp/lm/NGramModel.java
+++ code/nlp/lm/NGramModel.java
@@ -1,12 +1,29 @@
 package code.nlp.lm;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public abstract class NGramModel {
     
+    HashSet<NGram> vocab;
     HashMap<NGram, Double> ngram_map;
-    HashMap<NGram, Double> n_1gram_map;
+    HashMap<NGram, HashMap<NGram, Double>> n_1gram_map;
 
     public abstract void trainModel();
+
+    public double getNGramCount(NGram ng) {
+        return ngram_map.get(ng);
+    }
+
+    // to return count(aa) pass in the ngram "aad"
+    public double getN_1GramCount(NGram ng) {
+        NGram n_1gram = ng.getN_1Gram();
+        HashMap<NGram, Double> nestedMap = n_1gram_map.get(n_1gram);
+        double sum = 0.0;
+        for(NGram key : nestedMap.keySet()) {
+            sum += nestedMap.get(key);
+        }
+        return sum;
+    }
 
 }
