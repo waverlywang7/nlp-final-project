862379632d0865320031370f2481b8704a541016
NoahNevens <nnevens@g.hmc.edu>
2023-04-28 20:31:35 -0700

    changed the trigram model

--- code/nlp/lm/Smoothing.java
+++ code/nlp/lm/Smoothing.java
@@ -31,14 +31,13 @@ public class Smoothing {
     }
 
     public static void main(String[] args) {
-        TrigramModel model = new TrigramModel("data/sentences");
+        BigramModel model = new BigramModel("data/sentences");
         for (NGram key : model.n_1gram_map.keySet()) {
             for (NGram innerKey : model.n_1gram_map.get(key).keySet()) {
                 System.out.println(key.getNGramArrayList() + " " + innerKey.getNGramArrayList() + " " + model.n_1gram_map.get(key).get(innerKey));
             }
         }
         ArrayList<String> test_words = new ArrayList<String>();
-        test_words.add("of");
         test_words.add("the");
         test_words.add("station");
         Smoothing smoother = new Smoothing();
--- code/nlp/lm/TrigramModel.java
+++ code/nlp/lm/TrigramModel.java
@@ -22,12 +22,13 @@ public class TrigramModel extends NGramModel {
 
  HashMap<String, HashMap<String, Double>> bigram_map = new HashMap<>();
  ArrayList<String> words_encountered = new ArrayList<String>();
- ArrayList<String> unigram_vocab = new ArrayList<String>(); //TODO: num of words that occur at least 2 times, which will be the number of unique words including UNK. 
+ HashMap<String, Double> unigram_map = new HashMap<String, Double>(); // num of words encountered two times or more, the number of unique words including UNK. 
 
 
 
 
  public TrigramModel(String filename) {
+  unigram_map.put("<UNK>", 0.0);
    try {
      File myObj = new File(filename);
      Scanner myReader = new Scanner(myObj);
@@ -40,33 +41,42 @@ public class TrigramModel extends NGramModel {
      while (myReader.hasNextLine()) {
        String data = myReader.nextLine();
        data = "<s> " + data + " </s>";
-       String[] sentence = data.split("\\s+");
-       ArrayList<String> word_list = new ArrayList<String>();
-       for (String word : sentence) {
-           if (!words_encountered.contains(word)) {
-               word_list.add("<UNK>");
-               // TODO: if unigram_vocab does not contain UNK
-                  // add UNK to unigram_vocab
-               words_encountered.add(word);
-           } else {
-               word_list.add(word);
-              // TODO:
-              // if unigram_vocab does not contains word
-              // add word to unigram_vocab
-           }
-       }
+       for (String word : data.split("\\s+")) {
+        // create ngram object
+        // ArrayList<String> word_list = new ArrayList<String>(); // a word list that just contains one word
+        // word_list.add(word);
+        // NGram unigram = new NGram(word_list);
+
+        if (unigram_map.containsKey(word)) {
+          //unigram_map.put(word, unigram_map.get(word) + 1.0); // if word is already in hashmap, increment count
+
+          unigram_map.replace(word, unigram_map.get(word) + 1.0);
+          new_data.add(word); // add word to new_data
+          
+          
+        } else { // if word is not in hashmap, add to hashmap but set count to 0, and replace it
+                 // with <UNK>.
+          //unigram_map.put(word, 0.0); // keep track if we encountered word
+          
+          unigram_map.put(word, 0.0);
+          unigram_map.replace("<UNK>", unigram_map.get("<UNK>>") + 1.0);
+          new_data.add("<UNK>");
+          //unigram_map.put("<UNK>", unigram_map.get("<UNK>") + 1);// word is already in hashmap add to count
+
+        }
+      }
 
 
-       for (int i = 0; i < word_list.size()-2; i++) {
+       for (int i = 0; i < new_data.size()-2; i++) {
          ArrayList<String> ngram_words = new ArrayList<String>();
-         ngram_words.add(word_list.get(i));
-         ngram_words.add(word_list.get(i+1));
+         ngram_words.add(new_data.get(i));
+         ngram_words.add(new_data.get(i+1));
          NGram bigram = new NGram(ngram_words);
         
          //the third word is also an ngram
          ArrayList<String> third_word = new ArrayList<String>();
          NGram last_word = new NGram(third_word);
-         third_word.add(word_list.get(i+2));
+         third_word.add(new_data.get(i+2));
          if (!n_1gram_map.containsKey(bigram)) {
            n_1gram_map.put(bigram, new HashMap<NGram, Double>()); // if word is already in hashmap, increment count
          }
@@ -84,7 +94,7 @@ public class TrigramModel extends NGramModel {
          n_1gram_map.put(bigram, bigram_map);
 
 
-         ngram_words.add(word_list.get(i+2));
+         ngram_words.add(new_data.get(i+2));
          NGram trigram = new NGram(ngram_words);
          if (!ngram_map.containsKey(trigram)) {
            ngram_map.put(trigram, 0.0);
@@ -114,99 +124,13 @@ public class TrigramModel extends NGramModel {
  public static void main(String[] args) {
 
 
-   // null poiter execption maybe because i crate a model like this.
    TrigramModel model = new TrigramModel("data/training.txt");
-   //System.out.println(ngram_map);
-   //System.out.println(n_1gram_map);
 
-
-   // List<Double> list = new ArrayList<Double>();
-   // list.add(.99);
-   // list.add(.9);
-   // list.add(.75);
-   // list.add(.5);
-   // list.add(.25);
-   // list.add(.1);
-   // for (double item : list) {
-   //   System.out.println(item);
-   //   LambdaLMModel model = new LambdaLMModel("data/training.txt", item);
-
-
-   //   System.out.println(model.getPerplexity("data/development.txt"));
-   // }
  }
 
 
- // @Override
- // public double logProb(ArrayList<String> sentWords) {
 
 
- //   String current_word = "<s>";
- //   sentWords.add("</s>");
-
-
- //   double total = 0;
- //   double probability = 0;
-
-
- //   // calculate the log prob of a sentence.
- //   for (String word : sentWords) {
-
-
- //     probability = getBigramProb(current_word, word);
-
-
- //     total += Math.log10(probability); // add log prob
- //     current_word = word;
- //   }
- //   return total;
- // }
-
-
- // @Override
- // public double getPerplexity(String filename) {
- //   try {
- //     File myObj = new File(filename);
- //     Scanner myReader = new Scanner(myObj);
-
-
- //     double perplexity = 0;
- //     double total_log_prob = 0;
- //     int word_count = 0;
-
-
- //     // read through each line of text
- //     while (myReader.hasNextLine()) {
- //       String data = myReader.nextLine();
- //       ArrayList<String> new_data = new ArrayList<>();
-
-
- //       // split the text by white space
- //       String[] new_data_split = data.split("\\s+");
- //       Collections.addAll(new_data, new_data_split);
-
-
- //       // calculate log prob of sentence
- //       total_log_prob += logProb(new_data);
- //       word_count += new_data_split.length + 2; // we are accounting for sentence tags
- //     }
-
-
- //     perplexity = Math.pow(10, -1 * (total_log_prob / word_count));
-
-
- //     myReader.close();
- //     return perplexity;
-
-
- //   } catch (FileNotFoundException e) {
- //     System.out.println("An error occurred.");
- //     e.printStackTrace();
- //   }
-
-
- //   return 0;
- // }
 
 
 
